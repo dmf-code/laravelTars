260354c4f7b98111a8df5caf0dde1dded1262053
dmf-code <1015814408@qq.com>
2020-09-18 17:52:00 +0800

    fix: 修复链式查询无法使用where条件问题

--- src/app/Dao/BaseDao.php
+++ src/app/Dao/BaseDao.php
@@ -4,6 +4,7 @@
 namespace App\Dao;
 
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class BaseDao
@@ -12,8 +13,57 @@ abstract class BaseDao
     protected $params;
     public function __construct(Model $model)
     {
-        $this->model = $model;
+        $this->model = $model::query();;
         $this->params = [];
+    }
+
+    public function hasDateBetween($field="created_at", $start="date_start", $end="date_end")
+    {
+        if (
+            !is_null($startDate = request()->input($start, null)) &&
+            !is_null($endDate = request()->input($end, null))
+        ) {
+            $this->model->where($field, '>=', $startDate)
+                ->where($field, '<=', $endDate);
+        }
+
+        return $this;
+    }
+
+    public function hasDate($key = 'date', $field='created_at')
+    {
+        if (!is_null($date = request()->input($key, null))) {
+            switch ($date) {
+                case 'day':
+                    $this->model->whereDate($field, Carbon::today()->toDateString());
+                    break;
+                case '-day':
+                    $this->model->whereDate($field, Carbon::yesterday()->toDateString());
+                    break;
+                case '-7days':
+                    $this->model->whereDate($field, '>=', Carbon::parse('-7 days')->toDateString())
+                        ->whereDate($field, '<=', Carbon::now()->toDateString());
+                    break;
+                case '-15days':
+                    $this->model->whereDate($field, '>=', Carbon::parse('-15 days')->toDateString())
+                        ->whereDate($field, '<=', Carbon::now()->toDateString());
+                    break;
+                case '-30days':
+                    $this->model->whereDate($field, '>=', Carbon::parse('-30 days')->toDateString())
+                        ->whereDate($field, '<=', Carbon::now()->toDateString());
+                    break;
+                case '-1/2year':
+                    $this->model->whereDate($field, '>=', Carbon::parse('-6 months')->toDateString())
+                        ->whereDate($field, '<=', Carbon::now()->toDateString());
+                    break;
+                case '-year':
+                    $this->model->whereDate($field, '>=', Carbon::parse('-1 year')->toDateString())
+                        ->whereDate($field, '<=', Carbon::now()->toDateString());
+                    break;
+            }
+        }
+
+        return $this;
     }
 
     public function appendParam($key, $value=null)
@@ -60,6 +110,30 @@ abstract class BaseDao
             $value = sprintf($format, $param);
             $this->model->where($filed, 'like', $value);
         }
+        return $this;
+    }
+
+    public function hasIn($key, $field=null)
+    {
+        if (!is_null($item = request()->input($key, null))) {
+            if (is_null($field)) {
+                $field = $key;
+            }
+            $this->model->whereIn($field, explode(",", $item));
+        }
+
+        return $this;
+    }
+
+    public function hasEqual($key, $field=null)
+    {
+        if (!is_null($item = request()->input($key, null))) {
+            if (is_null($field)) {
+                $field = $key;
+            }
+            $this->model->where([$field => $item]);
+        }
+
         return $this;
     }
 
